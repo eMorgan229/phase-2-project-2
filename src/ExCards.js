64be6a887d9d778878bbee7672f0c1878db6ed3d
Em <ekramm@me.com>
2022-06-02 13:22:51 -0400

    intitial commit

--- src/ExCards.js
+++ src/ExCards.js
@@ -3,33 +3,18 @@ import React, {useState} from 'react'
 
 function ExCards({exCards}) {
     const {image, author, title} = exCards
-    const [isFavorited, setIsFavorited] = useState(false);
 
   return (
-    <li className="card">
+    <ul className="card">
       <div className="image">
         <img src={image} alt={author} />
       </div>
       <div className="details">
-        {isFavorited ? (
-          <button
-            onClick={() => setIsFavorited(false)}
-            className="emoji-button favorite active"
-          >
-            ★
-          </button>
-        ) : (
-          <button
-            onClick={() => setIsFavorited(true)}
-            className="emoji-button favorite"
-          >
-            ☆
-          </button>
-        )}
+        
         <strong>{author}</strong>
         <span> · {title}</span>
       </div>
-    </li>
+    </ul>
     
   )
 }
