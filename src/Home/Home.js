ad2a63cc6577c8d17528f1f4ad43c852a175d058
Em <ekramm@me.com>
2022-06-03 10:06:30 -0400

    fixed photos and other css

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -18,8 +18,10 @@ function Home() {
   return ( 
      <div className='page'>
         <h2 className='home-title'>Please Select 2 exhibitions to enjoy! </h2>
-        <div className='home-cards'><HomeCards photoData={photoData1} handleChange={handleChange1}/> 
-       <HomeCards photoData={photoData} handleChange={handleChange}/> </div>
+        <div className='home-cards'>
+          <HomeCards photoData={photoData1} handleChange={handleChange1}/> 
+          <HomeCards photoData={photoData} handleChange={handleChange}/> 
+       </div>
          
 
 
